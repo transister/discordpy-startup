beb099882393e4432821af18107bfcf8556ae5f7
transister <transister.ecb@gmail.com>
2021-02-25 22:13:30 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,6 +2,10 @@
 from discord.ext import commands
 import os
 import traceback
+import time
+import requests
+import json
+import copy
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
