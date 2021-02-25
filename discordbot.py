8d15db27d789200474b4990bb667935032b7d265
transister <transister.ecb@gmail.com>
2021-02-25 22:02:44 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -8,13 +8,6 @@ import json
 import copy
 from datetime import datetime, timedelta, timezone
 
-
-webhook_url_Hololive = 'https://discord.com/api/webhooks/814455705607077928/7GHQbsP_NNHR9uZ78CzVmiQwyCajJEdRpqI8hRDD-yKy87evrNnkEmJDiIOCT2nLMjHn' #ホロライブ配信開始
-webhook_url_Hololive_yotei = 'https://discord.com/api/webhooks/814455705607077928/7GHQbsP_NNHR9uZ78CzVmiQwyCajJEdRpqI8hRDD-yKy87evrNnkEmJDiIOCT2nLMjHn' #ホロライブ配信予定
-broadcast_data = {} #配信予定のデータを格納
-
-YOUTUBE_API_KEY = ["AIzaSyD1v807Gio9K4GVjKVjdRVgw0_kMip7z8Y","AIzaSyDYbIaTUq3yipQrOHncHhHjKDxVRZDZE5s","AIzaSyACZwmWNAyT5w2Spzm3_61Rw0GiH33utRU","AIzaSyDR5AhxSeIKsvIMJDqhsMTfh_fvo6DLR3o","AIzaSyCbmIAmPpKnLMrM2vEGg8MoqTyHgTVMAOM","AIzaSyDE9i7mg0ruYaISi8MPVH-tMd8LE4B_kNg","AIzaSyA2x_6iFWJHDlxjYAq4-ekMz9lHnlgZcAA"]
-
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
