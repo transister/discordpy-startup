a4366a34ae97d10c8df60cb00e7f1cfa8ff80943
transister <transister.ecb@gmail.com>
2021-02-26 06:58:55 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -132,7 +132,12 @@ def post_broadcast_schedule(userId, videoId, starttime):
         "content": content #文章
     }
     requests.post(webhook_url_Hololive_yotei, main_content) #Discordに送信
-
+    
+# 起動時に動作する処理
+@client.event
+async def on_ready():
+    # 起動したらターミナルにログイン通知が表示される
+    await ctx.send("起動しました")
 
 @bot.event
 async def on_command_error(ctx, error):
