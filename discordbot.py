42a67a0cb840b16f8b389a226166c9a08bcc930f
transister <transister.ecb@gmail.com>
2021-02-25 23:01:50 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -148,7 +148,7 @@ async def ping(ctx):
 bot.run(token)
 while True:
     now_time = datetime.now() + timedelta(hours=9)
-    if((now_time.minute == 0) and (now_time.hour % 1 == 0)):
+    if((now_time.minute == 5) and (now_time.hour % 1 == 0)):
         get_information()
     check_schedule(now_time, broadcast_data)
     time.sleep(60)
