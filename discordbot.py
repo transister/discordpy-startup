890480d28df39500c763d8997e7a62cc59e851ac
transister <transister.ecb@gmail.com>
2021-02-25 22:00:10 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -8,36 +8,6 @@ import json
 import copy
 from datetime import datetime, timedelta, timezone
 
-Hololive = {
-    "UChAnqc_AY5_I3Px5dig3X1Q": [
-        "戌神ころね",
-        "https://yt3.ggpht.com/ytc/AAUvwnimjdERaJDGopfH8UaB0r9tr_p8uyuEWWyYVkAd5Q=s88-c-k-c0x00ffffff-no-rj"
-    ],
-    "UCCzUftO8KOVkV4wQG1vkUvg": [
-        "宝鐘マリン",
-        "https://yt3.ggpht.com/ytc/AAUvwnjPuFWs42Vx2yIhK7z1w4L-e1GIpHn_5R1uknbS=s88-c-k-c0x00ffffff-no-rj"
-    ],
-    "UC1DCedRgGHBdm81E1llLhOQ": [
-        "兎田ぺこら",
-        "https://yt3.ggpht.com/ytc/AAUvwnjvkyPGzOmEXZ34mEFPlwMKTbCDl1ZkQ_HkxY-O5Q=s88-c-k-c0x00ffffff-no-rj"
-    ],
-    "UC5CwaMl1eIgY8h02uZw7u8A": [
-        "星街すいせい",
-        "https://yt3.ggpht.com/ytc/AAUvwnjdAl5rn3IjWzl55_0-skvKced7znPZRuPC5xLB=s88-c-k-c0x00ffffff-no-rj"
-    ],
-    "UCdn5BQ06XqgXoAxIhbqw5Rg": [
-        "白上フブキ",
-        "https://yt3.ggpht.com/ytc/AAUvwniEQsukDZoC-l8zchhRzpraxcH7Fyq9amrj2980Aw=s88-c-k-c0x00ffffff-no-rj"
-    ],
-    "UC1opHUrw8rvnsadT-iGp7Cg": [
-        "湊あくあ",
-        "https://yt3.ggpht.com/ytc/AAUvwngM9Jmc29dvbOY43w7RWFbOZLU4tGtOkEwtt-g7PA=s88-c-k-c0x00ffffff-no-rj"
-    ],
-    "UCvzGlP9oQwU--Y0r9id_jnA": [
-        "大空スバル",
-        "https://yt3.ggpht.com/ytc/AAUvwniCgko15I_x5bYWm0G2vnf5hZqD5hLOtLEDw0Na=s88-c-k-c0x00ffffff-no-rj"
-    ],
-} #配信者のチャンネルID, 配信者名, アイコン画像のURLのリスト
 
 webhook_url_Hololive = 'https://discord.com/api/webhooks/814455705607077928/7GHQbsP_NNHR9uZ78CzVmiQwyCajJEdRpqI8hRDD-yKy87evrNnkEmJDiIOCT2nLMjHn' #ホロライブ配信開始
 webhook_url_Hololive_yotei = 'https://discord.com/api/webhooks/814455705607077928/7GHQbsP_NNHR9uZ78CzVmiQwyCajJEdRpqI8hRDD-yKy87evrNnkEmJDiIOCT2nLMjHn' #ホロライブ配信予定
@@ -47,9 +17,6 @@ YOUTUBE_API_KEY = ["AIzaSyD1v807Gio9K4GVjKVjdRVgw0_kMip7z8Y","AIzaSyDYbIaTUq3yip
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
-
-
-
 
 @bot.event
 async def on_command_error(ctx, error):
