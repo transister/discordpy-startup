6d78ba99c5bf0be51afc514e0206207f3284cf9c
transister <transister.ecb@gmail.com>
2021-02-25 20:34:29 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -130,7 +130,7 @@ def post_broadcast_schedule(userId, videoId, starttime):
 
 while True:
     now_time = datetime.now() + timedelta(hours=9)
-    if(((now_time.year > 2020) or ((now_time.year == 2020) and (now_time.month >= 6) and (now_time.day >= 22))) and (now_time.minute == 30) and (now_time.hour %  == 0)):
+    if(((now_time.year > 2020) or ((now_time.year == 2020) and (now_time.month >= 6) and (now_time.day >= 22))) and (now_time.minute == 35) and (now_time.hour % 1 == 0)):
         get_information()
     check_schedule(now_time, broadcast_data)
     time.sleep(60)
