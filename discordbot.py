ab1e93c7a86c4a1e04523c8251a835de4c44efd8
transister <transister.ecb@gmail.com>
2021-03-03 12:59:05 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -107,13 +107,13 @@ async def get_information():
                     broadcast_data[video]['starttime'] = vd['items'][0]['liveStreamingDetails']['scheduledStartTime']
         except KeyError: #配信予定がなくて403が出た
             continue
-    for vi in broadcast_data:
-        if(not(vi in tmp)):
+    for vi in list(broadcast_data):
+        if(not(vi in list(tmp))):
             print(broadcast_data[vi])
             try:
                 sd_time = dataformat_for_pyton(broadcast_data[vi]['starttime']) #配信スタート時間をdatetime型で保管
                 sd_time += timedelta(hours=9)
-                if(now_time > sd_time):#今の方が配信開始時刻よりも先だったら
+                if(now_time < sd_time):#今の方が配信開始時刻よりも先だったら
                     post_broadcast_schedule(broadcast_data[vi]['channelId'], vi, broadcast_data[vi]['starttime'])
             except KeyError:
                 continue
