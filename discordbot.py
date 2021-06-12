7a8cf75595637f2a847c98b86858c31ce2b73d72
transister <transister.ecb@gmail.com>
2021-06-13 06:33:55 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -402,13 +402,13 @@ async def showTL():
         #tl = api.list_timeline(owner_screen_name='asuma_Noah', slug='774inc', count=10)
         tl.reverse()
         for status in tl:
-            tweet_data[status.author.id] = {'text':status.text}
-            tweet_data[status.author.id] = {'icon_url':status.author.profile_image_url}
-            tweet_data[status.author.id] = {'name':status.author.name}
+            tweet_data[status.id] = {'text':status.text}
+            tweet_data[status.id] = {'icon_url':status.author.profile_image_url}
+            tweet_data[status.id] = {'name':status.author.name}
             main_content = {    
-                    "username": tweet_data[status.author.id]['name'],
-                    "avatar_url": tweet_data[status.author.id]['icon_url'],
-                    "content": tweet_data[status.author.id]['text']
+                    "username": tweet_data[status.id]['name'],
+                    "avatar_url": tweet_data[status.id]['icon_url'],
+                    "content": tweet_data[status.id]['text']
             }
             requests.post(webhook_url, main_content)            
             #status.created_at += timedelta(hours=9) # add 9 hours for Japanese time
