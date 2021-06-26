087a40b49d4ae9c9c3af49de6a48cded9fa91cae
transister <transister.ecb@gmail.com>
2021-06-26 15:12:52 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -344,7 +344,7 @@ async def get_information():
     v_data = json.loads(aaa.text)
     api_link = "https://www.googleapis.com/youtube/v3/search?part=snippet&fields=items(id,snippet/title,snippet/channelId,snippet/publishedAt)&q=" + queryWord + "&key=" + YOUTUBE_API_KEY + "&eventType=live&type=video&maxResults=50"
     aaa = requests.get(api_link)
-    #v_data.update(json.loads(aaa.text))
+    v_data |= json.loads(aaa.text)
     for item in v_data['items']:#各配信予定動画データに関して
         try:
             if(item['snippet']['channelId'] in idList):
