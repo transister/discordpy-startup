9b80b21f8e9daebdb2f3b4ad0a52e6e2ce870c04
transister <transister.ecb@gmail.com>
2021-07-03 15:53:54 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -348,15 +348,17 @@ async def get_information():
     #v_data.update(**v_data2)
     
     try:
+        a = v_data['error']
+    except:
         for item in v_data['items']:#各配信予定動画データに関して
             try:
                 if(item['snippet']['channelId'] in idList):
                     broadcast_data[item['id']['videoId']] = {'channelId':item['snippet']['channelId']} #channelIDを格納
             except KeyError:
                 continue
-    except:
-        pass
     try:
+        aa = v_data2['error']
+    except:
         for item2 in v_data2['items']:#各配信予定動画データに関して
             try:
                 if(item2['snippet']['channelId'] in idList):
@@ -364,9 +366,7 @@ async def get_information():
                         broadcast_data[item2['id']['videoId']] = {'channelId':item2['snippet']['channelId']} #channelIDを格納
             except KeyError:
                 continue
-    except:
-        pass
-        
+    
     for video in broadcast_data:
         try:
             a = broadcast_data[video]['starttime'] #既にbroadcast_dataにstarttimeがあるかチェック
